c5fbd53f7415c2083d44f7297fd321593db5487f
jmacdissi <jmacdissi@gmail.com>
2015-12-02 16:51:37 -0600

    Initial

---
+++ Main.c
@@ -0,0 +1,17 @@
+#pragma config(Motor,  port1,           transmission,  tmotorVex393_HBridge, openLoop)
+#pragma config(Motor,  port2,           conv,          tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           LLaunch,       tmotorVex393TurboSpeed_MC29, openLoop, driveLeft)
+#pragma config(Motor,  port4,           RLaunch,       tmotorVex393TurboSpeed_MC29, openLoop, driveRight)
+#pragma config(Motor,  port5,           RLift1,        tmotorVex393_MC29, openLoop, driveRight)
+#pragma config(Motor,  port6,           RLift2,        tmotorVex393_MC29, openLoop, driveRight)
+#pragma config(Motor,  port7,           LLift1,        tmotorVex393_MC29, openLoop, driveLeft)
+#pragma config(Motor,  port8,           LLift2,        tmotorVex393_MC29, openLoop, driveLeft)
+#pragma config(Motor,  port9,           RDrive,        tmotorVex393_MC29, openLoop, driveRight)
+#pragma config(Motor,  port10,          LDrive,        tmotorVex393_HBridge, openLoop, driveLeft)
+
+task main()
+{
+
+
+
+}
